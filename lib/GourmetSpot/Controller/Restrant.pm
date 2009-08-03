f9b6736b7dcd065e4e95506d1434ec63ba7cc490
Nobuo Danjou <nobuo.danjou@gmail.com>
2009-08-03 14:04:11 +0900

    oops! there is infinite loop!

--- lib/GourmetSpot/Controller/Restrant.pm
+++ lib/GourmetSpot/Controller/Restrant.pm
@@ -58,6 +58,7 @@ sub setup_item_params :Private {
             if ($item->{id}) {
                 unless ($item->{holiday} || $item->{pre_holiday} || scalar $item->{day_of_week}) {
                     $item->{_delete} = 1;
+                    $i++;
                     next;
                 }
             }
@@ -73,45 +74,6 @@ sub setup_item_params :Private {
         }
     }
 }
-
-#sub create_item :Private {
-#    my ( $self, $c ) = @_;
-#
-#    $self->next::method($c);
-#    $c->forward('update_openhours');
-#}
-#
-#sub update_item :Private {
-#    my ( $self, $c ) = @_;
-#
-#    $self->next::method($c);
-#    $c->forward('update_openhours');
-#}
-#
-#sub update_openhours :Private {
-#    my ( $self, $c ) = @_;
-#
-#    $c->log->_dump( $c->stash->{outer_params} );
-#
-#    for my $item (@{$c->stash->{outer_params}->{'DBIC::OpenHours'}}) {
-#        $item or next;
-#        $item->{restrant_id} = $c->stash->{item}->id;
-#        $item->{day_of_week} ||= [];
-#        $item->{holiday} ||= 0;
-#        $item->{pre_holiday} ||= 0;
-#        for (qw(opens_at closes_at)) {
-#            $item->{$_} = join(':', 
-#                delete $item->{"${_}_hour"} || '00', 
-#                delete $item->{"${_}_minute"} || '00',
-#            );
-#        }
-#        if ($item->{holiday} || $item->{pre_holiday} || scalar $item->{day_of_week}) {
-#            $c->model('DBIC::OpenHours')->update_or_create($item);
-#        } else {
-#            $c->model('DBIC::OpenHours')->find($item->{id})->delete if $item->{id};
-#        }
-#    }
-#}
 
 =head2 index
 
